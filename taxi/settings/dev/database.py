3572c8a8b90fdc21aae2fc9ab4babbb0e8720f7b
Acheron <acheron.igor.b@gmail.com>
2018-01-22 03:51:14 +0200

    Finally signals bug fixed

--- taxi/settings/dev/database.py
+++ taxi/settings/dev/database.py
@@ -4,6 +4,8 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES = {}
 DATABASES['default'] = db_from_env
 
+# Uncomment for local use
+
 # import os
 #
 # DB_NAME = os.environ["DB_NAME"]
